2fc19ff9d52ff297598bc8c49595a43692dee7c0
amisha-0705 <130228911+amisha-0705@users.noreply.github.com>
2023-04-25 18:37:19 +0530

    Add files via upload

---
+++ date.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int dd,mm,yy;
+    printf("entert the dd/mm/yyyy: ");
+    scanf("%d/%d/%d",&dd,&mm,&yy);
+    printf("date- %d ,month- %d ,year- %d ",dd,mm,yy);
+    return 0;
+}
